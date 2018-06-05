6b522f68aa7b6126c2bcd85f02ae0ae023e92659
Ryan Noronha <ryannoronha1@Ryans-MacBook-Pro.local>
2018-06-05 12:07:13 -0300

    removed sendgrid API key, added to gitignore

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,6 +5,6 @@ const validation = require("./validation");
 const User = require("../../src/db/models").User;
 
 router.get("/users/sign_up", userController.signUp);
-router.post("/users/sign_up", validation.validateUsers, userController.create);
+router.post("/users/sign_ups", validation.validateUsers, userController.create);
 
 module.exports = router;
