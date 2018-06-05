83678bea6cd5e095e29acab3237b55ab5547573f
Ryan Noronha <ryannoronha1@Ryans-MacBook-Pro.local>
2018-06-05 14:35:56 -0300

    Sign in and out working, need to fix signing in with wrong password

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,6 +5,9 @@ const validation = require("./validation");
 const User = require("../../src/db/models").User;
 
 router.get("/users/sign_up", userController.signUp);
-router.post("/users/sign_ups", validation.validateUsers, userController.create);
+router.post("/users/sign_up", validation.validateUsers, userController.create);
+router.get("/users/sign_in", userController.signInForm);
+router.post("/users/sign_in", validation.validateUsers, userController.signIn);
+router.get("/users/sign_out", userController.signOut);
 
 module.exports = router;
