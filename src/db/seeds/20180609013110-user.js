529082384ec1f6049d96190c88118c48a98d8f81
Ryan Noronha <ryannoronha1@Ryans-MacBook-Pro.local>
2018-06-09 12:14:38 -0300

    Seeding Data: completed

---
+++ src/db/seeds/20180609013110-user.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const faker = require("faker");
+
+let users = [
+   {
+    name: "Sean Connery",
+    email: "sean@mi6.com",
+    password: "sean007",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    role: 0
+   },
+   {
+    name: "Roger Moore",
+    email: "roger@mi6.com",
+    password: "roger007",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    role: 1
+   },
+   {
+    name: "Pierce Brosnan",
+    email: "pierce@mi6.com",
+    password: "pierce007",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    role: 2
+   }
+ ]
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert("Users", users, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete("Users", null, {});
+  }
+};
